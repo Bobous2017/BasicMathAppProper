3610ca7f731926604e9409a6b5213e7b56010bcc
B-2017 <107534702+Bobous2017@users.noreply.github.com>
2025-08-05 19:23:31 +0200

    Add project files.

---
+++ BasicMathAppProper/BasicMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathAppProper
+{
+    public static class BasicMath
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Subtract(int a, int b) => a - b;
+        public static int Multiply(int a, int b) => a * b;
+        public static int Divide(int a, int b) => a / b; // Note: No zero check here
+    }
+}
+
